3709ebc45217814a5eff5c9283ce492660004fa7
Nawinkishore <nawinkishore2004@gmail.com>
2024-01-19 20:32:18 +0530

    update

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,25 +2,59 @@ import express from 'express';
 import conn from '../config/db.js';
 import sqlString from 'sqlstring'; 
 const router =  express.Router();
-router.get('/',(req,res)=>{
-    // res.send("Welcome");
-    let query = sqlString.format('SELECT sentence from question');
-    conn.query(query,(err,result)=>{
-        if(err){
-            console.log(err);
-            return res.json({
-                success:false,
-                message:"There was a problem"
+
+router.get('/', async (req, res) => {
+    try {
+        const query = sqlString.format('SELECT * FROM question');
+        const questions = await new Promise((resolve, reject) => {
+            conn.query(query, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    reject({
+                        success: false,
+                        message: "There was a problem"
+                    });
+                }
+                resolve(result);
             });
-        }
-        let list = result.map(item=>item.sentence);
-        return res.json({
-            success:true,
-            message:"successfully created",
-            list,
         });
-    });
-})
+
+        const questionPromises = questions.map(async (question) => {
+            const optionQuery = sqlString.format(
+                `SELECT * FROM questionoption WHERE questionId = ${question.questionId}`
+            );
+
+            try {
+                const options = await new Promise((resolve, reject) => {
+                    conn.query(optionQuery, (err, result) => {
+                        if (err) {
+                            reject({
+                                success: false,
+                                message: "There was a problem"
+                            });
+                        }
+                        resolve(result);
+                    });
+                });
+
+                question.options = options;
+                return question;
+            } catch (error) {
+                throw error;
+            }
+        });
+
+        const updatedQuestions = await Promise.all(questionPromises);
+
+        return res.json({
+            success: true,
+            questions: updatedQuestions
+        });
+    } catch (error) {
+        return res.json(error);
+    }
+});
+
 router.get('/',(req, res)=>{
     // let query = sqlString.format('SELECT 
 })
